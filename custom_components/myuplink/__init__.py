f79de424b7fa3467e1c9145b79dc652d98e2cbdf
kayjei <kay.lidenalv@gmail.com>
2022-01-17 21:50:57 +0100

    Moving from sensor platform to component

--- custom_components/myuplink/__init__.py
+++ custom_components/myuplink/__init__.py
@@ -1,1 +1,46 @@
-"""Sensor integration for MyUplink."""
+"""Integration for MyUplink."""
+import logging
+import sys
+import voluptuous as vol
+from .connect import myUplink
+
+import homeassistant.helpers.config_validation as cv
+from homeassistant.const import (CONF_API_KEY, CONF_API_TOKEN, CONF_REGION)
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+
+PLATFORMS = [Platform.CLIMATE, Platform.SENSOR, Platform.NUMBER]
+
+DOMAIN = "myuplink"
+
+_LOGGER = logging.getLogger(__name__)
+
+CONFIG_SCHEMA = vol.Schema({
+  DOMAIN: {
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Required(CONF_API_TOKEN): cv.string,
+    vol.Optional(CONF_REGION, default="en-US"): cv.string
+  },
+}, extra=vol.ALLOW_EXTRA)
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Share data and load platforms."""
+    
+    _LOGGER.debug("Initializing component: myUplink ...")
+
+    hass.data[DOMAIN] = {
+        'api_key': config[DOMAIN]['api_key'],
+        'api_token': config[DOMAIN]['api_token'],
+        'region': config[DOMAIN]['region']
+    }
+
+    if DOMAIN not in config:
+        return True
+  
+    myUplink.authenticate(config[DOMAIN])
+    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+    hass.helpers.discovery.load_platform('number', DOMAIN, {}, config)
+
+    return True
