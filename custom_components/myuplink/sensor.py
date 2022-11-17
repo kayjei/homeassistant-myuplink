bad5d3a79f718a3b3d46aefeee94a643d32378e5
kayjei <kay.lidenalv@gmail.com>
2022-11-17 22:40:11 +0000

    Adding unit_of_measurement

--- custom_components/myuplink/sensor.py
+++ custom_components/myuplink/sensor.py
@@ -86,14 +86,9 @@ class myUplinkSensor(Entity):
         return self._name
 
     @property
-    def native_unit_of_measurement(self) -> str:
+    def unit_of_measurement(self):
         """Return the unit of measurement."""
         return self._unit
-
-    @property
-    def temperature_unit(self):
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
 
     @property
     def device_class(self):
