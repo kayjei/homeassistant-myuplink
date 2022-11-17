a9a3143a374cf05470446c2f6adcf269a715b139
K-J Lidenalv <kay.lidenalv@gmail.com>
2022-11-17 22:22:57 +0100

    Update sensor.py

--- custom_components/myuplink/sensor.py
+++ custom_components/myuplink/sensor.py
@@ -86,7 +86,7 @@ class myUplinkSensor(Entity):
         return self._name
 
     @property
-    def native_unit_of_measurement(self) -> str:
+    def unit_of_measurement(self) -> str:
         """Return the unit of measurement."""
         return self._unit
 
